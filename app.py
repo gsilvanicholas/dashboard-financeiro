5fe94bb70386e0fc11f5ab84552c0b82f082a29a
gsilvanicholas <gsilva.nicholas@gmail.com>
2026-09-05 22:39:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import requests
 import streamlit.components.v1 as components
 
 # 1. CONFIGURAÇÃO DA PÁGINA (Layout Profissional Wide)
